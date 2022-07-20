14afc7f9820a183252cf2e2a964fa86b709e6c3c
deepthy041 <prasaddeepthy9@gmail.com>
2022-07-19 22:10:11 -0700

    checking

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -691,8 +691,8 @@ module.exports = {
                     "payment_method": "paypal"
                 },
                 "redirect_urls": {
-                    "return_url": "http://localhost:3000/success",
-                    "cancel_url": "http://localhost:3000/cancel"
+                    "return_url": "http://deepthyprasad.com/success",
+                    "cancel_url": "http://deepthyprasad.com/cancel"
                 },
                 "transactions": [{
                     "item_list": {
